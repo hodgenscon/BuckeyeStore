df380745b1358830edde1f7a2217ee420219ed44
Connor Hodgens <Connor Hodgens>
2017-04-17 18:13:56 -0400

    step 12 2

--- Source/BuckeyeStore.Core/Product.cs
+++ Source/BuckeyeStore.Core/Product.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BuckeyeStore.Core
 {
-    public class Class1
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public string Description { get; set; }
+        public string Brand {get; set;}
+        public Review Review { get; set;}
+        public AggregateRating AggregateRating {get; set;}
+        public List<Offer> Offers {get; set;}
+        public string Sku {get; set;}
     }
 }
